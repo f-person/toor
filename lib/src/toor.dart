73ac6e26c33bc198ca9776b37fc2841693be25ed
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2022-06-04 20:21:41 +0400

    feat: Implement LazySingleton

--- lib/src/toor.dart
+++ lib/src/toor.dart
@@ -2,15 +2,42 @@ part 'toor_impl.dart';
 
 typedef FactoryFunc<T> = T Function();
 
+typedef LazySingletonFunc<T> = T Function();
+
 abstract class Toor {
   static late final Toor _instance = _ToorImpl();
 
-  /// access to the Singleton instance of Toor
+  /// Access to the Singleton instance of Toor.
   static Toor get instance => _instance;
 
+  /// Registers a factory (i. e. a new instance of [T] will be created via
+  /// [factoryFunc] on every call of [ToorFactory.create()]).
   ToorFactory<T> registerFactory<T>(FactoryFunc<T> factoryFunc);
+
+  /// Registers a singleton which will be created once called.
+  ToorLazySingleton<T> registerLazySingleton<T>(
+    LazySingletonFunc<T> lazySingletonFunc,
+  );
 }
 
 abstract class ToorFactory<T> {
+  const ToorFactory();
+
+  /// Creates a new instance of [T].
   T create();
+
+  T call() => create();
+}
+
+abstract class ToorLazySingleton<T> {
+  const ToorLazySingleton();
+
+  /// Returns the created instance of [T]. If an instance is not created
+  /// yet, a new one will be created.
+  T get();
+
+  T call() => get();
+
+  /// Deletes the current instance of [T].
+  void reset();
 }
--- lib/src/toor_impl.dart
+++ lib/src/toor_impl.dart
@@ -5,13 +5,38 @@ class _ToorImpl extends Toor {
   ToorFactory<T> registerFactory<T>(FactoryFunc<T> factoryFunc) {
     return _ToorFactoryImpl(factoryFunc);
   }
+
+  @override
+  ToorLazySingleton<T> registerLazySingleton<T>(
+    LazySingletonFunc<T> lazySingletonFunc,
+  ) {
+    return _ToorLazySingletonImpl(lazySingletonFunc);
+  }
 }
 
-class _ToorFactoryImpl<T> implements ToorFactory<T> {
+class _ToorFactoryImpl<T> extends ToorFactory<T> {
   const _ToorFactoryImpl(this.factoryFunc);
 
   final FactoryFunc<T> factoryFunc;
 
   @override
   T create() => factoryFunc();
+}
+
+class _ToorLazySingletonImpl<T> extends ToorLazySingleton<T> {
+  _ToorLazySingletonImpl(this.lazySingletonFunc);
+
+  final LazySingletonFunc lazySingletonFunc;
+
+  T? _instance;
+
+  @override
+  T get() {
+    return _instance ??= lazySingletonFunc();
+  }
+
+  @override
+  void reset() {
+    _instance = null;
+  }
 }
