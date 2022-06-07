7d6292245eed9be6a58cbe77ddf52ef66bade662
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2022-06-07 14:32:39 +0400

    feat: Use lazy initialization instead of nullable fields in _ToorLazySingletonImpl

--- lib/src/toor_impl.dart
+++ lib/src/toor_impl.dart
@@ -29,17 +29,29 @@ class _ToorFactoryImpl<T> extends ToorLocator<T> {
 class _ToorLazySingletonImpl<T> extends ToorLocator<T> {
   _ToorLazySingletonImpl(this.lazySingletonCreator);
 
-  final FactoryFunc lazySingletonCreator;
+  final FactoryFunc<T> lazySingletonCreator;
 
-  T? _instance;
+  late _ToorLazySingletonHolder<T> _instanceHolder = _createInstanceHolder();
 
   @override
   T get() {
-    return _instance ??= lazySingletonCreator();
+    return _instanceHolder.instance;
   }
 
   @override
-  void dispose() {
-    _instance = null;
+  void reset() {
+    _instanceHolder = _createInstanceHolder();
   }
+
+  _ToorLazySingletonHolder<T> _createInstanceHolder() {
+    return _ToorLazySingletonHolder<T>(lazySingletonCreator);
+  }
+}
+
+class _ToorLazySingletonHolder<T> {
+  _ToorLazySingletonHolder(this.lazySingletonCreator);
+
+  final FactoryFunc<T> lazySingletonCreator;
+
+  late T instance = lazySingletonCreator();
 }
